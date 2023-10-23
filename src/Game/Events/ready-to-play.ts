93bec2e0ac75cfefed24f6a0e4f36c0c1aad271d
Lucas Losekann <lucas1losekann@gmail.com>
2023-10-23 20:50:34 -0300

    Opponent name

--- src/Game/Events/ready-to-play.ts
+++ src/Game/Events/ready-to-play.ts
@@ -1,14 +1,29 @@
 import { JwtPayload } from "jsonwebtoken";
 import { Socket } from "socket.io";
 import WsService from "../../Services/WsService";
+import prisma from "../../db";
 
 export default function readyToPlay(socket: Socket, user: JwtPayload){
-    return () => {
+    return async () => {
         const game = Array.from(WsService.ongoingGames.values()).find(ps => ps.player1 === user.id || ps.player2 === user.id)
         if (!game) return;
         console.log(user.id, ' ready to play')
 
         socket.join(`${game.player1}-${game.player2}`)
+
+        const opponent = await prisma.user.findUnique({
+            where: {
+                id: game.player1 === user.id ? game.player2 : game.player1
+            },
+            select: {
+                name: true
+            }
+        })
+
+        socket.emit('game-info', {
+            opponentName: opponent?.name
+        })
+
 
         WsService.io.to(`${game.player1}-${game.player2}`).emit('game-update', {
             board: game.board,
